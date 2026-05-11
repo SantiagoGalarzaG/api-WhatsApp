9e1c85e3af361e2ccd260753ce2823e8e120177f
Deyvid Santiago Galarza <deyvidgalarzag04@gmail.com>
2026-05-10 21:31:11 -0500

    realizando ajustes en los controladores

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,5 +1,20 @@
 const VerifyToken = (req, res) => {
-    res.send("Token verificado correctamente");
+    try{
+        let accessTocken = "WSP_9fK2xQ7LmN4vT8aZ1pR6uD3yH5cJ0sE"
+        let token = req.query["hub.verify_token"];
+        let challenge = req.body["hub.challenge"];
+
+        if(challenge !=null && token !=null && accessTocken == token){
+            res.send(challenge);
+        }else{
+            res.status(400).send("Token no verificado");
+        }
+
+
+
+    }catch(error){
+        res.status(400).send("Error al verificar el token");
+    }
 }
 
 const ReceivedMessage = (req, res) => {
